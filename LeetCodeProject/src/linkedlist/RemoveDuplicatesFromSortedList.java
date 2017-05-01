a7e0fe1f9f99ab136cd1424fd5cca9b218e91aaf
jfq <jfqiao.123@qq.com>
2017-05-01 17:08:43 +0800

    linked list, 2017/05/01

--- LeetCodeProject/src/linkedlist/RemoveDuplicatesFromSortedList.java
+++ LeetCodeProject/src/linkedlist/RemoveDuplicatesFromSortedList.java
@@ -3,24 +3,8 @@ package linkedlist;
 public class RemoveDuplicatesFromSortedList {
 	public static void main(String[] args) {
 		RemoveDuplicatesFromSortedList rdfsl = new RemoveDuplicatesFromSortedList();
-		ListNode l1 = new ListNode(1);
-		ListNode l2 = new ListNode(2);
-		ListNode l3 = new ListNode(3);
-		ListNode l4 = new ListNode(3);
-		ListNode l5 = new ListNode(4);
-		ListNode l6 = new ListNode(4);
-		ListNode l7 = new ListNode(5);
-		ListNode l8 = new ListNode(5);
-		ListNode l9 = new ListNode(6);
-		l1.next = l2;
-		l2.next = l3;
-		l3.next = l4;
-		l4.next = l5;
-		l5.next = l6;
-		l6.next = l7;
-		l7.next = l8;
-		l8.next = l9;
-		ListNode head = rdfsl.deleteDuplicates(null);
+		ListNode head = ListNode.generateListNode();
+		head = rdfsl.deleteDuplicates(head);
 		ListNode.output(head);
 	}
 
